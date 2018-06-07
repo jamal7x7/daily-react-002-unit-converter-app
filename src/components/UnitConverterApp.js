13fb159f7ac16d637970100d5aeac303fd13aae5
jamal7X7 <jamal7x7@gmail.com>
2018-06-07 12:28:42 +0000

    =onSubmit worked on select

--- src/components/UnitConverterApp.js
+++ src/components/UnitConverterApp.js
@@ -11,8 +11,8 @@ const Header = (props) => (
 )
 
 const Options = (props) => (
-  <select name="length1" size="1">             
-   {props.units.length.map( x =>  <option key={x}> {x}</option> )}              
+  <select name="length" size="1" onChange={props.convertFromUnitHandler}>             
+   {props.units.length.map( x =>  <option key={x} name={x}> {x}</option> )}              
   </select>
 )
 
@@ -20,10 +20,18 @@ const Converter = (props) => (
   <div className='convertContainer'>
 
           <div className='from'>
-            <p>from</p>
-            <form onChange={props.convertFromHandler}>
-              <input type='text' name='from' />
-              <Options units={props.units} /> 
+            <p>from </p>
+            <form   >
+              <input 
+                type='text' 
+                name='fromTextInput' 
+                onChange={props.convertFromHandler}
+                defaultValue={1} 
+              /> 
+              <Options 
+               units={props.units} 
+               convertFromUnitHandler={props.convertFromUnitHandler}
+               /> 
             </form>       
           </div>
 
@@ -33,9 +41,9 @@ const Converter = (props) => (
           </div>
 
           <div className='to'>
-            <p>to</p>
+            <p>to {props.result}</p>
             <form >
-              <input type='text' name='to' />
+              <input type='text' name='toTextInput' />
               <Options units={props.units} />
             </form>
           </div>
@@ -47,32 +55,47 @@ const Converter = (props) => (
 class UnitConverterApp extends Component {
   state = {
     units : {
-      length : ['mm', 'cm', 'm','in','ft-us', 'ft', 'ml'],
+      length : ['mm', 'cm', 'm','in','ft-us', 'ft', 'mi'],
       area: ['mm2', 'cm2', 'm2', 'in2', 'ha', 'km2','ft2','ac', 'ml2'], 
       mass: ['mcg', 'mg', 'g', 'kg', 'oz', 'lb', 'mt', 't'],
-      temperature: ['C', 'F', 'K', 'R'],
-      
-    }
+      temperature: ['C', 'F', 'K', 'R'],  
+    },
+    unitPicked: 'mm',
+    valEntered: 1
+  }
+
+  
+  convertFromUnitHandler = (e) => {
+    e.preventDefault()
+    const u = e.target.value
+    console.log('convertFromUnitHandler called', u)
+    this.setState( () => ({
+      unitPicked: u
+    }))
   }
 
   convertFromHandler = (e) => {
     e.preventDefault()
-    const v = e.target.elements.from.value.trim()
-    console.log(v)
+    const val = e.target.value
+    console.log('convertFromHandler called', val)
+    this.setState(() => ({
+      valEntered: val
+    }))
   }
 
-
-
+  
   render() {
     return (
       <div className="App">
-        
+      
         <Header />
-
         <Converter 
           units={this.state.units}
           convertFromHandler={this.convertFromHandler}
+          convertFromUnitHandler={this.convertFromUnitHandler}
+          result={convert(this.state.valEntered).from(this.state.unitPicked).to('m')}
         />
+
       </div>
     )
   }
